91421c4826f6ea2c919505351c32af83c5d1d14c
William Campbell <i34170@3ecorp.com>
2025-08-05 11:34:53 -0400

    run db in prod when using launch

--- launch.bat
+++ launch.bat
@@ -1,6 +1,6 @@
 @echo off
-echo "Starting backend server..."
-start "Backend" cmd /k "cd backend && npm start"
+echo "Starting backend server in production mode..."
+start "Backend" cmd /k "cd backend && set NODE_ENV=production&& npm start"
 
 echo "Starting frontend server..."
-start "Frontend" cmd /k "cd frontend && npm start"
+start "Frontend" cmd /k "cd frontend && npm run serve"
